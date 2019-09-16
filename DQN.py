386edaf5a75d4bae011913b75c944b29e9c8ef54
Kaan <kubilaykagankomurcu@gmail.com>
2019-09-17 00:34:07 +0300

    Variables initialized, Network is close to be complete

--- DQN.py
+++ DQN.py
@@ -2,28 +2,82 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-
+import tensorflow as tf
 
 class DQN:
-    def __innit__(self, action, state, next_state, gamma=0.1):
+    def __innit__(self, env, states, actions, rewards, states_next, gamma=0.5):
+        super().__innit__()
         self.gamma = gamma
+       # self.unstoppable_gamma = gamma
+        self.env = env
+        self.states = states
+        self.states_next = states_next
+        self.actions = actions
+        self.rewards = rewards
         self.N = 0
 
+
+    def initialize_environment(self):
+        env = gym.make('CartPole-v0')
+        return env
+
+
+    def initialize_observation(self):
+        observation = self.env.reset()
+        observation_size = observation.shape[0]
+        return observation, observation_size
+
+
+    def initialize_network(self, observation_size):
+        batch_size = 50
+
+        states = tf.placeholder(tf.float32, shape=(batch_size, observation_size), name='state')
+        states_next = tf.placeholder(tf.float32, shape=(batch_size, observation_size), name='state_next')
+        actions = tf.placeholder(tf.int32, shape=(batch_size,), name='action')
+        rewards = tf.placeholder(tf.float32, shape=(batch_size,), name='reward')
+        #done = tf.placeholder(tf.float32, shape=(batch_size,), name='done')
+
+        buffer = [states, actions, rewards, states_next]
+
+        return buffer
+
+    def CartPoleNetwork(self, actions, rewards):
+        Q = tf.layers.dense(self.states, [64,64,2], activation= tf.nn.relu)
+        Q_next = tf.layers.dense(self.states_next, [64,64,2], activation= tf.nn.relu)
+        one_hot_vector_actions = tf.one_hot(actions, 2)
+        pred = tf.reduce_sum(Q * one_hot_vector_actions) #TODO how does this create predictions  ?
+
+
+
+        Q_opt = rewards + self.gamma *  tf.reduce_max(Q)        #TODO terminal states shouldn't get gamma terms, fix this.
+        Q_next_opt = rewards + self.gamma *  tf.reduce_max(Q_next)
+
+
+        loss =tf.reduce_mean(tf.square(pred - Q_next_opt))
+        optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
+
+
     def EpsGreedy(self, eps_start=1):
-        self.N += 1
-        eps = eps_start/np.log2(self.N+1)
+
+        eps = eps_start/(self.N+1)
         prob = np.random.random()
 
         if prob < eps:
-            # random choice
+            action = self.env.action_space.sample() # random choice
         else:
-            # optimal choice
+            action = argmax()                       #TODO optimal choice
 
-    def DeepQNetwork(self, state, next_state):
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        Quality = reward + self.gamma * # (approximation of max future reward) #
+        self.N += 1
+        return action
 
+    def DeepQ(self, ):
+
+        action = self.EpsGreedy()
+        observation, reward, done, info = self.env.step(action)
+        Q = reward + self.gamma *  #TODO (approximation of max future reward) #
+
+
+'''
 env = gym.make('CartPole-v0')
 
 for i_episode in range(20):
@@ -41,3 +95,5 @@ for i_episode in range(20):
             break
 
 env.close()
+'''
+
