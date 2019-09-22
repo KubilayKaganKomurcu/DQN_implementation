28dcda22df5bc751a7f91fce005e69a9fefc7c5f
Kaan <kubilaykagankomurcu@gmail.com>
2019-09-22 11:58:17 +0300

    moved initialization functions into _init_

--- DQN.py
+++ DQN.py
@@ -18,7 +18,7 @@ class dense_network:
 
 
 class DQN:
-    def __init__(self, layer_sizes, input_size, output_size, session,
+    def __init__(self, env, layer_sizes, input_size, output_size, session,
                  max_experiences=10000, min_experiences=50, gamma=0.9, batch_size=50):
         self.gamma = gamma
         self.batch_size = batch_size
@@ -27,35 +27,41 @@ class DQN:
         self.experience = {'states': [], 'actions': [], 'rewards': [], 'states_next': [], 'done': []}
         self.N = 0
         self.session = session
+        self.observation = env.reset()
+        self.observation_size = self.observation.shape[0]
+
 
         self.layers = []
         first_layer = dense_network(input_size, layer_sizes[0])
         self.layers.append(first_layer)
         middle_layer = dense_network(layer_sizes[0], layer_sizes[1])
         self.layers.append(middle_layer)
-        final_layer = dense_network(layer_sizes[1], output_size)
+        final_layer = dense_network(layer_sizes[1], output_size, lambda x: x)
         self.layers.append(final_layer)
 
         self.parameters = []
         for layer in self.layers:
             self.parameters += layer.parameters
+        
+        self.states = tf.placeholder(tf.float32, shape=(None, input_size), name='state')
+        self.actions = tf.placeholder(tf.int32, shape=(None, ), name='action')
+        self.targets = tf.placeholder(tf.float32, shape=(None, ), name='target')
 
-    def initialize_observation(self, env):
-        observation = env.reset()
-        observation_size = observation.shape[0]
-        return observation, observation_size
+        for layer in self.layers:
+            states = layer.forward(self.states)
 
-    def initialize_network(self, input_size):
-        states = tf.placeholder(tf.float32, shape=(None, input_size), name='state')
-        states_next = tf.placeholder(tf.float32, shape=(None, input_size), name='state_next')
-        actions = tf.placeholder(tf.int32, shape=(None), name='action')
-        rewards = tf.placeholder(tf.float32, shape=(None), name='reward')
-        # done = tf.placeholder(tf.float32, shape=(None,), name='done')
-        targets = tf.placeholder(tf.float32, shape=(None), name='target')
+        self.calculated_net = states
 
-        buffer = [states, actions, rewards, states_next, targets]
+        one_hot_vector_actions = tf.one_hot(self.actions, 2)
+        selected_action = tf.reduce_sum(self.targets * one_hot_vector_actions, reduction_indices=[1])
 
-        return buffer
+        # Q_opt = rewards + self.gamma * tf.reduce_max(Q)
+        # Q_next_opt = rewards + self.gamma * tf.reduce_max(Q_next)
+
+        loss = tf.reduce_sum(tf.square(self.calculated_net - selected_action))
+        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
+
+    
 
     def add_experience(self, states, actions, rewards, states_next, done):
         if len(self.experience['states']) >= self.max_experiences:
@@ -70,29 +76,14 @@ class DQN:
         self.experience['states_next'].append(states_next)
         self.experience['done'].append(done)
 
-    def Optimizer(self, buffer):
 
-        states = buffer[0]
-        actions = buffer[1]
-        targets = buffer[4]
 
-        for layer in self.layers:
-            states = layer.forward(states)
 
-        calculated_net = states
 
-        one_hot_vector_actions = tf.one_hot(actions, 2)
-        selected_action = tf.reduce_sum(targets * one_hot_vector_actions, reduction_indices=[1])
 
-        # Q_opt = rewards + self.gamma * tf.reduce_max(Q)
-        # Q_next_opt = rewards + self.gamma * tf.reduce_max(Q_next)
 
-        loss = tf.reduce_sum(tf.square(calculated_net - selected_action))
-        optimizer = tf.train.AdamOptimizer(0.001).minimize(loss)
 
-        return calculated_net, optimizer
-
-    def EpsGreedy(self, buffer, calculated_net, observation, env, eps_start=1):
+    def EpsGreedy(self, observation, env, eps_start=1):
 
         eps = eps_start / (np.sqrt(self.N + 1))
         prob = np.random.random()
@@ -101,17 +92,17 @@ class DQN:
             action = env.action_space.sample()
         else:
             obs = np.atleast_2d(observation)
-            action = np.argmax(self.predict(obs, calculated_net, buffer)[0])
+            action = np.argmax(self.predict(obs)[0])
 
         self.N += 1
         return action
 
-    def predict(self, state, calculated_net, buffer):
-        actions = buffer[0]
-        two_d_state = np.atleast_2d(state)
-        return self.session.run(calculated_net, feed_dict={actions: two_d_state})
+    def predict(self, state):
 
-    def train(self, target, optimizer, buffer):
+        two_d_state = np.atleast_2d(state)
+        return self.session.run(self.calculated_net, feed_dict={self.actions: two_d_state})
+
+    def train(self, target):
 
         if len(self.experience['states']) < self.min_experiences:
             return
@@ -127,10 +118,10 @@ class DQN:
         next_Q = np.max(target.predict(next_states), axis=1)
         targets = [r + self.gamma * next_q if not done else r for r, next_q, done in zip(rewards, next_Q, dones)]
 
-        self.session.run(optimizer, feed_dict={
-            buffer[0]: states,
-            buffer[1]: actions,
-            buffer[4]: targets}
+        self.session.run(self.optimizer, feed_dict={
+            self.states: states,
+            self.actions: actions,
+            self.targets: targets}
                          )
 
     def copy_parameters(self, other):
@@ -145,20 +136,20 @@ class DQN:
         self.session.run(ops)
 
 
-def play_one(env, model, train_model, eps, copy_period, optimizer, buffer, calculated_net):
+def play_one(env, model, train_model, eps, copy_period):
     observation = env.reset()
     done = False
     total_reward = 0
     i = 0
     while not done and i < 2000:
-        action = model.EpsGreedy(buffer, calculated_net, observation, env, eps)
+        action = model.EpsGreedy(observation, env, eps)
         prev_observation = observation
         observation, reward, done, info = env.step(action)
 
         total_reward += reward
 
         model.add_experience(prev_observation, action, reward, observation, done)
-        model.train(train_model, optimizer, buffer)
+        model.train(train_model)
 
         i += 1
 
@@ -179,26 +170,22 @@ if __name__ == '__main__':
     init = tf.global_variables_initializer()
     session = tf.InteractiveSession()
 
-    model = DQN(layer_sizes, input_size, output_size, session)
-    observation_model, observation_size_model = model.initialize_observation(env)
+    model = DQN(env, layer_sizes, input_size, output_size, session)
 
-    buffer = model.initialize_network(observation_size_model)
-    calculated_net, optimizer = model.Optimizer(buffer)
-
-    train_model = DQN(layer_sizes, input_size, output_size, session)
-    observation_train_model, observation_size_train_model = train_model.initialize_observation(env)
+    train_model = DQN(env, layer_sizes, input_size, output_size, session)
 
 
 
 
 
-    train_model = DQN(layer_sizes, input_size, output_size, session)
+
+
 
     N = 500
     total_rewards = np.empty(N)
     for n in range(N):
         eps_start = 1.0
-        totalreward = play_one(env, model, train_model, eps_start, copy_period, optimizer, buffer, calculated_net)
+        totalreward = play_one(env, model, train_model, eps_start, copy_period)
         total_rewards[n] = totalreward
         if n % 100 == 0:
             print("episode:", n, "total reward:", totalreward, "avg reward (last 100):",
@@ -232,7 +219,14 @@ for i_episode in range(20):
 
 env.close()
 
+def initialize_network(self, input_size):
+        states_next = tf.placeholder(tf.float32, shape=(None, input_size), name='state_next')
+        rewards = tf.placeholder(tf.float32, shape=(None), name='reward')
+        # done = tf.placeholder(tf.float32, shape=(None,), name='done')
 
+        buffer = [states, actions, rewards, states_next, targets]
+
+        return buffer
 
     def One_Round(self):
         action = self.env.action_space.sample()
